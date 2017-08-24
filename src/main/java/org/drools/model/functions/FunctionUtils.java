ee3f5a87c29783baf1c02ea7b8283041bcf776e8
Mario Fusco <mario.fusco@gmail.com>
2017-08-24 19:32:20 +0200

    minor refactor

--- src/main/java/org/drools/model/functions/FunctionUtils.java
+++ src/main/java/org/drools/model/functions/FunctionUtils.java
@@ -5,29 +5,14 @@ public final class FunctionUtils {
     private FunctionUtils() { }
 
     public static <R> FunctionN<R> toFunctionN(final Function0<R> f) {
-        return new FunctionN<R>() {
-            @Override
-            public R apply(Object... objs) {
-                return f.apply();
-            }
-        };
+        return objs -> f.apply();
     }
 
     public static <A, R> FunctionN<R> toFunctionN(final Function1<A, R> f) {
-        return new FunctionN<R>() {
-            @Override
-            public R apply(Object... objs) {
-                return f.apply((A)objs[0]);
-            }
-        };
+        return objs -> f.apply( (A)objs[0] );
     }
 
     public static <A, B, R> FunctionN<R> toFunctionN(final Function2<A, B, R> f) {
-        return new FunctionN<R>() {
-            @Override
-            public R apply(Object... objs) {
-                return f.apply((A)objs[0], (B)objs[1]);
-            }
-        };
+        return objs -> f.apply( (A)objs[0], (B)objs[1] );
     }
 }
--- src/main/java/org/drools/model/functions/PredicateN.java
+++ src/main/java/org/drools/model/functions/PredicateN.java
@@ -3,10 +3,5 @@ package org.drools.model.functions;
 public interface PredicateN {
     boolean test(Object... objs);
 
-    PredicateN True = new PredicateN() {
-        @Override
-        public boolean test(Object... objs) {
-            return true;
-        }
-    };
+    PredicateN True = objs -> true;
 }

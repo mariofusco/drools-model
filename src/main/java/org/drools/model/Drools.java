0159f5164731af6ce36673f0c1214a45c97b9901
Mario Fusco <mario.fusco@gmail.com>
2017-08-29 15:06:54 +0200

    add support for working memory actions

--- src/main/java/org/drools/model/Drools.java
+++ src/main/java/org/drools/model/Drools.java
@@ -3,7 +3,9 @@ package org.drools.model;
 public interface Drools {
     void insert(Object object);
 
-    void update(Object object);
+    void update(Object object, String... modifiedProperties);
+
+    void update(Object object, BitMask modifiedProperties);
 
     void delete(Object object);
 }
